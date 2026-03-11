cb18c88fa02759b1740bffaa9aed0c642a701873
pocketparty2 <ilikefindtheshampoos@gmail.com>
2026-03-11 23:55:32 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
-function createGame() {
-  alert("Game created! (we will add rooms later)");
+function generateRoomCode(){
+return Math.random().toString(36).substring(2,7).toUpperCase()
 }
 
-function joinGame() {
-  alert("Joining game!");
+function createGame(){
+
+let room = generateRoomCode()
+
+document.getElementById("game").innerHTML =
+"Room Code: " + room + "<br>Share this with friends!"
+
+}
+
+function joinGame(){
+
+let room = document.getElementById("roomInput").value
+
+document.getElementById("game").innerHTML =
+"Joining room " + room
+
 }
